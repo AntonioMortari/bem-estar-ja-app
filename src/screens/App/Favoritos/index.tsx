46435551c6d17aa8a831038813db8d45bba42824
matheusdonizetiferreira <mathferreira726@gmail.com>
2024-07-24 14:04:03 -0300

    Estilização da tela de favoritos

--- src/screens/App/Favoritos/index.tsx
+++ src/screens/App/Favoritos/index.tsx
@@ -1,10 +1,57 @@
-import { Text } from 'react-native-paper';
-
+import { SafeAreaView, View } from "react-native";
+import { theme } from "@/theme/paper";
+import { Searchbar, Text, SegmentedButtons } from "react-native-paper";
+import { styles } from "./styles";
+import React from "react";
 
 const Favoritos = () => {
-    return (
-        <Text variant='titleLarge'>Favoritos</Text>
-    );
-}
+  return (
+    
 
-export { Favoritos };
+
+    <View style={styles.containerHeader}>
+        
+        <Text variant="headlineMedium" style={{ color: theme.colors.light }}>
+          Meus Favoritos
+        </Text>
+
+      <View style={styles.containerBusca}>
+        <Searchbar
+          style={styles.inputBusca}
+          placeholder="Faça sua pesquisa"
+          elevation={3}
+          placeholderTextColor={theme.colors.gray}
+          value={""}
+        />
+      </View>
+    </View>
+    
+  );
+};
+
+const ServicosEProfissionais = () => {
+    const [value, setValue] = React.useState('boolean');
+
+    return (
+        <SafeAreaView style={styles.containerSegmentos}>
+          <SegmentedButtons
+            value={value}
+            onValueChange={setValue}
+            buttons={[
+              {
+                value: 'Serviços',
+                label: 'Serviços',
+              },
+              {
+                value: 'Profissionais',
+                label: 'Profissionais',
+              }
+            ]}
+          />
+        </SafeAreaView>
+      );
+    };
+
+
+
+export { Favoritos, ServicosEProfissionais };
